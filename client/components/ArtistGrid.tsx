66ff8e94e3d43a9168afcd1dd0a9359c7df9968f
russell-pollari <pollarir@mgail.com>
2023-06-17 12:47:09 -0400

    Responsive grid sizes

--- client/components/ArtistGrid.tsx
+++ client/components/ArtistGrid.tsx
@@ -7,9 +7,9 @@ import type { Artist } from '../types';
 function TopArtists({ artists = [] }: { artists: Artist[] }) {
   return (
     <Container>
-      <Grid container spacing={2} sx={{ marginTop: 2 }}>
+      <Grid container spacing={2} sx={{ marginTop: 2, flexGrow: 1 }}>
         {artists.map((artist) => (
-          <Grid item xs={4} key={artist.id}>
+          <Grid item xs={12} sm={8} md={4} key={artist.id}>
             <ArtistCard artist={artist} />
           </Grid>
         ))}
