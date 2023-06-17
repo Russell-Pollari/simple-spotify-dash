2e26b377ba4c2ec359de75c0bc68ea1e17c92e87
russell-pollari <pollarir@mgail.com>
2023-06-17 12:36:25 -0400

    Add placeholder for favourites

--- client/components/Favourites.tsx
+++ client/components/Favourites.tsx
@@ -4,12 +4,19 @@ import { useSelector } from 'react-redux';
 import PageContainer from './PageContainer';
 import ArtistGrid from './ArtistGrid';
 import type { RootState } from '../store';
+import { Link } from 'react-router-dom';
 
 function Favourites() {
   const favourites = useSelector((state: RootState) => state.favourites);
 
   return (
     <PageContainer title="Favourites">
+      {favourites.length === 0 && (
+        <p>
+          You don&apos;t have any favourites yet. Go to the{' '}
+          <Link to="/top-artists">Top Artists</Link> page to add some.
+        </p>
+      )}
       <ArtistGrid artists={favourites} />
     </PageContainer>
   );
